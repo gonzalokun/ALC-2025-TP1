b98606bb01505d9151cee82f7e0243145fa0b011
Gonzalo Springhart <glspringhart@gmail.com>
2025-11-09 22:41:32 -0300

    Algoritmos

--- src/alc.py
+++ src/alc.py
@@ -1,7 +1,10 @@
+from wsgiref.util import request_uri
+
 import numpy as np
 
 # TODO: traer todos los metodos en la seccion de abajo cuando estén corregidos
-from moduloALC import multiplicar, matricesIguales, esSimetrica, traspuesta
+from moduloALC import multiplicar, matricesIguales, esSimetrica, traspuesta, inversa, svd_reducida, calculaQR, calculaLU, esSDP, calculaLDV
+from src.moduloALC import calcularAx, res_tri
 
 
 # Funciones del Labo
@@ -13,6 +16,47 @@ def multiplicar_en_cadena(*lista):
         res = multiplicar(res, matriz)
 
     return res
+
+def aplicar_raiz_a_diagonal(A):
+    res = A
+    for i in range(res.shape[0]):
+        res[i][i] = np.sqrt(res[i][i])
+    return res
+
+def calcular_rango(A):
+    _, res_gauss, _ = calculaLU(A)
+    rank = 0
+    for i in range(res_gauss.shape[0]):
+        todosCeros = True
+        for j in range(res_gauss.shape[1]):
+            if res_gauss[i][j] != 0:
+                todosCeros = False
+
+        if not todosCeros:
+            rank += 1
+
+    return rank
+
+def calculaCholesky(A, atol=1e-10):
+    if not esSDP(A, atol):
+        return None
+
+    L, D, _ = calculaLDV(A)
+
+    R = multiplicar(L, aplicar_raiz_a_diagonal(D))
+
+    return R, traspuesta(R)
+
+def resolver_sistema_con_cholesky(A, B, atol=1e-10):
+    R, Rt = calculaCholesky(A, atol)
+
+    matriz_solucion = []
+    for columna in range(B.shape[1]):
+        yi = res_tri(R, B[:, columna])
+        xi = res_tri(Rt, yi, False)
+        matriz_solucion.append(xi)
+
+    return np.array(matriz_solucion)
 
 # Funciones TP
 
@@ -56,6 +100,101 @@ def cargarDataset(carpeta):
     Yv = traspuesta(np.array(Yv))
 
     return Xt, Yt, Xv, Yv
+
+# 1
+
+def pinvEcuacionesLineales(L, Y):
+
+    Lt = traspuesta(L)
+    matriz_solucion = []
+    for columna in range(Lt.shape[1]):
+        xi = res_tri(L, Lt[:, columna])
+        ui = res_tri(Lt, xi, False)
+        matriz_solucion.append(ui)
+
+    return np.array(matriz_solucion)
+
+def algoritmo1(X, Y):
+    # W = Y*L+
+    rango = calcular_rango(X)
+    Xt = traspuesta(X)
+
+    # Caso 1
+    n, p = X.shape
+    if rango == p and n > p:
+        XtX = multiplicar(Xt, X)
+        L, _ = calculaCholesky(XtX)
+        U = pinvEcuacionesLineales(L, Xt)
+        return multiplicar(Y, U)
+
+    # Caso 2
+    if rango == n and n < p:
+        XXt = multiplicar(X, traspuesta(X))
+        L, _ = calculaCholesky(traspuesta(XXt))
+        Vt = pinvEcuacionesLineales(L, X)
+        return multiplicar(Y, traspuesta(Vt))
+
+    # Caso 3
+    if rango == n and n == p:
+        invX = inversa(X)
+        return multiplicar(Y, invX)
+
+    print("ERROR HORRIBLE")
+    return None
+
+# 2
+
+def algoritmo2(X, Y):
+    #Pre Condicion: X tiene dimension nxp y n<p, rango(X)=n (rango completo) y Y dimension m×p (matrices en los reales)
+    U, S, V = svd_reducida(X, k="max", tol=1e-15)
+    W = pinvSVD(U, S, V, Y)
+    return W
+
+
+def pinvSVD(U, S, V, Y):
+    #calcula W  minW : ||Y-WX||_2
+
+    S1_inv = np.zeros((S.shape[1], S.shape[0]))
+
+    for i in range(S.shape[0]):
+        if S[i, i] > 1e-15:
+            S1_inv[i, i] = 1 / S[i, i]
+
+    U1 = U[:][:S.shape[1]]  #sospecho que como usamos el algoritmo svd_reducida, U ya tiene las columnas correctas pero x las dudas las cortamos
+    V1 = V[:][:S.shape[0]]
+    U1_t = traspuesta(U1)
+
+    pinvX = multiplicar_en_cadena(V1, S1_inv, traspuesta(U1_t))
+
+    W = multiplicar_en_cadena(Y,pinvX)
+    return W
+
+# 3
+
+def pinvHouseHolder(Q, R, Y):
+    X_plus = multiplicar(Q,inversa(traspuesta(R)))
+    V = X_plus
+    return multiplicar(Y,V)
+
+def pinvGramSchmidt(Q, R, Y):
+    X_plus = multiplicar(Q, inversa(traspuesta(R)))
+    V = X_plus
+    return multiplicar(Y, V)
+
+def algoritmo3(X, Y, metodo="RH"):
+
+    if metodo not in ["RH", "GS"]:
+        return None
+
+    Q,R = calculaQR(traspuesta(X), metodo)
+
+    if metodo == "RH":
+        return pinvHouseHolder(Q, R, Y)
+
+    if metodo == "GS":
+        return pinvGramSchmidt(Q, R, Y)
+
+# 4
 
 def esPseudoInversa(X, pX, tol=1e-8):
     return matricesIguales(multiplicar_en_cadena(X, pX, X), X, tol) \
